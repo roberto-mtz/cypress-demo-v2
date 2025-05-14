fefe8eb8ecce55fa1b67cc43ad9bbf4689629807
Roberto Martínez <robertomartinez@Robertos-MacBook-Air.local>
2025-05-13 18:43:40 -0600

    Cambio el demoqa a eliminar test

--- cypress/e2e/sample_demo_spec.cy.js
+++ cypress/e2e/sample_demo_spec.cy.js
@@ -1,10 +1,17 @@
 Cypress.on('uncaught:exception', (err, runnable) => {
-  return false
+  return false // Ignora errores JS no controlados en la página
 })
 
 describe('Formulario Demo QA', () => {
   it('Llenar y validar formulario', () => {
-    cy.visit('https://demoqa.com/text-box', { timeout: 120000 })
+    cy.visit('https://demoqa.com/text-box', {
+      timeout: 120000,
+      failOnStatusCode: false,
+      onBeforeLoad: (win) => {
+        delete win.fetch // Previene errores por fetch bloqueado en CI/CD
+      }
+    })
+
     cy.get('#userName').type('Juan Perez')
     cy.get('#userEmail').type('juan@mail.com')
     cy.get('#currentAddress').type('Direccion actual')
